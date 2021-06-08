50902d7823d2ba5057eb7c5c5226a03c41020fb4
Musthofa DS <musthofads@gmail.com>
2021-06-08 16:58:53 +0700

    tes

--- web_absensi/web_absensi/settings.py
+++ web_absensi/web_absensi/settings.py
@@ -15,14 +15,14 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+print(">>", BASE_DIR)
 #DataFlair #Django #Static files
 STATIC_URL = '/static/'
 #--------------------------------------------------
 STATIC_ROOT = os.path.join(BASE_DIR, 'web_absensi/static')
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'web_absensi/static'),
-]
+# STATICFILES_DIRS = [
+#         os.path.join(BASE_DIR, 'web_absensi/static'),
+# ]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
